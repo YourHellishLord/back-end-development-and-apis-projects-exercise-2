88f2a6535be95900f0d6bb7447ef714dfa478c86
Mauro De Rosa <it.mauro.derosa@gmail.com>
2024-09-16 18:44:39 +0000

    implemented whoami service endpoint

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -23,6 +24,15 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/whoami', (req, res) => {
+  const whoami = {
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  }
+  res.json(whoami);
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
